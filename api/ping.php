e8f5f9240146001ad946cdb6dcef8cb660c2da8d
Dayatabdi <dayatabdi7690@gmail.com>
2026-06-16 00:25:35 +0700

    Update ping.php

--- api/ping.php
+++ api/ping.php
@@ -4,7 +4,7 @@ require_once 'db_config.php';
 // Membuka koneksi ke Aiven
 $conn = getConnection();
 
-// Melakukan query super ringan cuma buat ngasih tau Aiven "Hei, aku masih hidup!"
+
 if ($conn->query("SELECT 1")) {
     echo json_encode(["status" => "success", "message" => "Database is awake!"]);
 } else {
